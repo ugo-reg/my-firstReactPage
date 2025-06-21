d6808f2c860dbaad0ca04a5ac63090ffde09edf1
ugo-reg <ugochi209@gmail.com>
2025-06-21 04:24:12 -0700

    edited heading

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,10 @@ function Header(){
     return(
         <>
         <h1>New Movies Review</h1>
-        <p> every week we reviwe movies for you, we also show you the choicest movies for the month.</p>
+        <p>HI 👋👋<br></br> Every week we review movies for you as well as update you on the choicest and most interesting movies for the week. 
+            It is always a pleasure🥰 to have you with us. Don't forget to leave us a comment and also visit our page✌️.Let us also know the genre of movies you love💖 to see more of✌️:<br>
+            </br>Horror😱🧟‍♀️<br></br>Romance😘😻<br></br>Animations<br></br>Cartoons🚸<br></br>Family👨‍👩‍👧‍👦
+        </p>
         </>
     )
 }
